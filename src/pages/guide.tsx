f378b339f7df3bba067b1ffea2653599d95703ea
Emmanuel-Develops <itakpeemma@gmail.com>
2023-10-03 19:42:34 +0100

    feat: new copy

--- src/pages/guide.tsx
+++ src/pages/guide.tsx
@@ -7,52 +7,185 @@ const QuickStart = () => {
   return (
     <main className="w-full font-inter text-xl px-10 py-10 md:max-w-7xl mx-auto">
       <Image src={race_car} alt="race_car" />
-      <section className="text-gray-800 font-normal">
-        <h1 className="text-5xl pb-4 text-brand-primary font-bold">
+      <section className="text-gray-800 font-normal pb-10">
+        <h1 className="text-5xl pb-6 text-brand-primary font-bold">
           Quick Start
         </h1>
-        <p className="pb-4">
-          LNSim is built to help you simulate lightning payments on a given
-          lightning network. Typically, this will be a regtest or signet
-          environment, but LNSim is agnostic about the network you use.
+        <div className="flex flex-col gap-4">
+          <p>
+            LNSim is built to help you simulate lightning payments on any
+            regtest or simnet network. To get started you will need:
+          </p>
+          <ul className="list-disc list-inside">
+            <li>
+              <span className="text-lg text-slate-700 font-medium">
+                A lightning network with open channels.
+              </span>
+            </li>
+            <li>
+              <span className="text-lg text-slate-700 font-medium">
+                Access to execute grpc commands on network nodes.
+              </span>
+            </li>
+            <li>
+              <span className="text-lg text-slate-700 font-medium">
+                The rust compiler installed.
+              </span>
+            </li>
+          </ul>
+          <p>
+            If you need help setting up a test network, we suggest{" "}
+            <span>
+              <Link
+                className="underline underline-offset-2 text-brand-primary"
+                href="https://lightningpolar.com/"
+                target="_blank"
+              >
+                Polar
+              </Link>
+            </span>{" "}
+            for beginners or Scaling Lightning for larger deployments.
+          </p>
+        </div>
+      </section>
+
+      <section className="text-gray-800 font-normal pb-10">
+        <h2 className="text-4xl pb-6 text-brand-primary font-bold">
+          How to Use
+        </h2>
+        <div className="flex flex-col gap-4">
+          <p>The basic steps for using SimLN are:</p>
+          <ul className="list-disc list-inside">
+            <li>
+              <span className="text-lg text-slate-700 font-medium">
+                Clone repo and install sim-ln.
+              </span>
+            </li>
+            <li>
+              <span className="text-lg text-slate-700 font-medium">
+                Configure access to nodes in a simple config.json.
+              </span>
+            </li>
+            <li>
+              <span className="text-lg text-slate-700 font-medium">
+                Define your desired activity
+              </span>
+            </li>
+            <li>
+              <span className="text-lg text-slate-700 font-medium">
+                Run the simulation!
+              </span>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="text-gray-800 font-normal pb-10">
+        <h2 className="text-4xl pb-4 text-brand-primary font-bold">
+          Install SimLN
+        </h2>
+        <p>
+          See our installation instructions on github to get started!{" "}
+          <span>
+            <Link
+              href="https://github.com/bitcoin-dev-project/sim-ln#getting-started"
+              className="underline underline-offset-1 text-brand-primary"
+            >
+              installation instructions
+            </Link>
+          </span>
         </p>
-        <div className="pb-4">
-          You can use a tool like{" "}
-          <Link
-            className="underline underline-offset-2 text-brand-primary"
-            href="https://lightningpolar.com/"
-            target="_blank"
-          >
-            Polar
-          </Link>{" "}
-          to help you quickly setup some regtest lightning nodes.
+      </section>
+      <section className="text-gray-800 font-normal pb-10">
+        <h2 className="text-4xl pb-4 text-brand-primary font-bold">
+          Get in Touch
+        </h2>
+        <div className="flex flex-col gap-4">
+          <div>
+            <p>Having problems running sim-ln?</p>
+            <p>
+              <span>Get in touch with us on </span>
+              <span>
+                <Link
+                  href="https://github.com/bitcoin-dev-project/sim-ln/discussions"
+                  className="underline underline-offset-1 text-brand-primary"
+                >
+                  Github Discussions
+                </Link>
+              </span>
+            </p>
+          </div>
+          <div>
+            <p>Found a bug or want a feature?</p>
+            <p>
+              <span>Please feel free to </span>
+              <span>
+                <Link
+                  href="https://github.com/bitcoin-dev-project/sim-ln/issues/new"
+                  className="underline underline-offset-1 text-brand-primary"
+                >
+                  open up an issue
+                </Link>
+              </span>
+              <span> with bug reports or feature requests.</span>
+            </p>
+          </div>
+          <div>
+            <p>Want to contribute to the project?</p>
+            <p>
+              If you’d like to contribute code, maintenance help or
+              documentation to the project please open up a volunteer issue and
+              we’ll find something fun for you.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-800 font-normal pb-10">
+        <h2 className="text-4xl pb-4 text-brand-primary font-bold">
+          Copy Experiment
+        </h2>
+        <div className="flex flex-col gap-4">
+          <p>
+            Look at your lightning regtest network. It&apos;s so lonely. Just Alice
+            and Bob. Occasionally, Charlie stops by, but very rarely these days.
+            And when he does, things are just awkward. What should Alice, Bob,
+            and Charlie talk about? Maybe simulate a single lightning payment?
+            Wow, you really know how to spice things up Charlie.
+          </p>
+          <p>
+            Life is short. Our time as nodes in this plane of existence is but a
+            fleeting moment in the vastness of time. Why waste it with empty,
+            dull, stagnant lightning networks?
+          </p>
+          <p>
+            SimLN is here to make you feel again – to bring joy, wonder, awe,
+            nay, the entire palette of emotions back to your lightning
+            development experience. Simulate a payment from Alice to any number
+            of NPCs. Set it to recur every minute. Or every 10 seconds. Or scale
+            this up to every NPC in your network.
+          </p>
+          <p>
+            Your lightning network is a canvas, and simulations are a rich
+            tapestry that you paint with every keystroke into your <Code>config.toml</Code>
+            file.
+          </p>
+          <p>
+            SimLN – because life is too short to have a boring lightning
+            network.
+          </p>
         </div>
       </section>
       <section>
-        <h2 className="text-4xl pb-4 text-brand-primary font-bold">How to Use</h2>
-        <p className="pb-4">The basic steps for using LnSim are:</p>
-        <ul className='list-disc list-inside'>
-          <li>
-            <span>Download LnSim</span>
-          </li>
-          <li>
-            <span>Configuration</span>
-            <ul className='ml-4 list-disc list-inside'>
-              <li>Add your node credentials</li>
-              <li>Define your simulation</li>
-            </ul>
-          </li>
-          <li>Run the simulation</li>
-        </ul>
-      </section>
-      <section>
-        <h2 className="text-4xl pb-4 text-brand-primary font-bold">Download LNSim</h2>
-        <p className="pb-4">You can <Link href="https://github.com/bitcoin-dev-project/sim-ln" className="underline underline-offset-1 text-brand-primary">download the latest LNSim binary here</Link></p>
-        <p className="pb-4">Optionally, you can <Link href="https://github.com/bitcoin-dev-project/sim-ln" className="underline underline-offset-1 text-brand-primary">clone from GitHub</Link> and compile from source if you prefer.</p>
-      </section>
-      <section>
-        <h2 className="text-4xl pb-4 text-brand-primary font-bold">Configuration</h2>
-        <p className="pb-4">Create a config.json file in the same directory as your LNSim binary. Inside, you’ll need to add the credentials for one of the lightning node’s in your network. You’ll also need to define the activity, which is what your simulation should do <Link href="/">See here </Link> for more in-depth docs on defining your simulation</p>
+        <h2 className="text-4xl pb-4 text-brand-primary font-bold">
+          Configuration
+        </h2>
+        <p className="pb-4">
+          Create a config.json file in the same directory as your SimLN binary.
+          Inside, you’ll need to add the credentials for one of the lightning
+          node’s in your network. You’ll also need to define the activity, which
+          is what your simulation should do <Link href="/">See here </Link> for
+          more in-depth docs on defining your simulation
+        </p>
       </section>
       <ConfigFileAnnotation />
     </main>
@@ -82,5 +215,15 @@ const ConfigFileAnnotation = () => {
         <p>{`}]`}</p>
       </div>
     </div>
+  );
+};
+
+const Code = ({children}: {children: string}) => {
+  return (
+    <>
+    <span> </span>
+    <code className="bg-gray-100 border-gray-300 border-[1px] rounded-md text-gray-600 font-semibold px-2 inline-flex py-0 text-[14px]">{children}</code>
+    <span> </span>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,135 @@
-import Image from 'next/image'
-import SimLnHero from '../../public/SimLNLogoASCII.svg'
-import ln_spec_dev from '../../public/svg/human_1.svg'
-import app_dev from '../../public/svg/human_2.svg'
-import bolt from '../../public/svg/bolt.svg'
-import Link from 'next/link'
-import { heroText } from '@/data'
+import Image from "next/image";
+import SimLnHero from "../../public/SimLNLogoASCII.svg";
+import ln_spec_dev from "../../public/svg/human_1.svg";
+import app_dev from "../../public/svg/human_2.svg";
+import bolt from "../../public/svg/bolt.svg";
+import Link from "next/link";
+import { heroText } from "@/data";
+import styles from "@/styles/home.module.scss";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center`}
-    >
-      <section className='w-full flex flex-col items-center gap-9 py-20 relative overflow-hidden'>
-        <p className='absolute text-lg text-gray-300 -z-10 left-0 top-0 w-full'>{heroText}</p>
-        <Image src={SimLnHero} alt='logo' />
-        <p className='text-5xl md:text-6xl text-brand-primary text-center max-w-[70%] font-inter font-[700]'>Instantly simulate real-world Lightning network activity</p>
-        <div className='font-press flex gap-9'>
-          <Link href="/guide" className='bg-brand-primary text-brand-secondary text-lg md:text-2xl p-3 md:p-6'>
-            Quick Start
-          </Link>
-          <Link href="https://github.com/bitcoin-dev-project/sim-ln" className='bg-brand-primary text-white text-lg md:text-2xl p-3 md:p-6'>
-            GitHub
-          </Link>
+    <main className={`flex min-h-screen flex-col items-center`}>
+      <section className="w-full relative overflow-hidden">
+        <p className="absolute text-lg text-gray-300 -z-10 left-0 top-0 w-full opacity-40">
+          {heroText}
+        </p>
+        <div className="w-full flex flex-col items-center gap-4 md:gap-9 md:py-20">
+          <Image src={SimLnHero} alt="logo" />
+          <p
+            className={`${styles.shimmer_text} text-5xl md:text-6xl text-brand-primary text-balance text-center md:w-[70%] lg:max-w-[800px] font-inter font-[700]`}
+          >
+            Instantly simulate real-world Lightning network activity
+          </p>
+          <div className="w-full text-center font-press justify-center flex gap-4 md:gap-9 px-2">
+            <Link
+              href="/guide"
+              className="bg-brand-primary text-brand-secondary text-md sm:text-lg md:text-2xl overflow-clip"
+            >
+              <span className={`${styles.action_buttons} p-3 md:p-6`}>
+                Quick Start
+              </span>
+            </Link>
+            <Link
+              href="https://github.com/bitcoin-dev-project/sim-ln"
+              className="bg-brand-primary text-brand-secondary text-md sm:text-lg md:text-2xl overflow-clip"
+            >
+              <span className={`${styles.action_buttons} p-3 md:p-6`}>
+                GitHub
+              </span>
+            </Link>
+          </div>
         </div>
       </section>
-      <section className='flex flex-col container mx-auto px-10 py-10 w-full md:w-[70%] lg:max-w-[800px]'>
-        <p className='mb-4 text-2xl text-slate-800'>Plenty of solutions exist for quickly creating lightning nodes in test environments. These can be useful for local development. However, without the strain of real-world lightning traffic, these testing tools do not provide a developer with an understanding of how their code will perform on mainnet.</p>
-        <p className='mb-4 text-2xl text-slate-800'>Lnsim fixes this by letting you simulate lightning activity as it might happen in the real world:</p>
-        <ul className='list-disc list-inside'>
+      <section className="flex flex-col container mx-auto px-10 py-10 w-full max-w-5xl">
+        <p className="mb-4 text-2xl text-gray-700">
+          Testing lightning applications and protocol changes in realistic
+          conditions is challenging. While many solutions exist for creating
+          regtest and simnet networks, producing regular payments within these
+          networks requires custom scripting and constant attention.
+        </p>
+        <p className="mb-4 text-2xl text-gray-700">
+          SimLN fixes this by providing the ability to simulate payment activity
+          on any test network! Features include:
+        </p>
+        <ul className="list-disc list-inside">
           <li>
-            <span className='text-lg text-slate-700 font-medium'>Routing nodes with many channels</span>
+            <span className="text-lg text-slate-700 font-medium">
+              Routing nodes with many channels
+            </span>
           </li>
           <li>
-            <span className='text-lg text-slate-700 font-medium'>Gossip between large quantities of peers </span>
+            <span className="text-lg text-slate-700 font-medium">
+              Gossip between large quantities of peers{" "}
+            </span>
           </li>
           <li>
-            <span className='text-lg text-slate-700 font-medium'>Routing multiple payments between</span>
+            <span className="text-lg text-slate-700 font-medium">
+              Routing multiple payments between
+            </span>
           </li>
         </ul>
       </section>
-      <section className="flex flex-col w-full px-10 py-10 max-w-7xl gap-4 mb-10">
+      <section className="flex flex-col w-full px-10 py-10 max-w-7xl gap-4">
         <div className="flex flex-col md:flex-row w-full gap-4">
-          <div className='flex basis-1/2 flex-col gap-2 '>
+          <div className="flex basis-1/2 flex-col gap-2 ">
             <Image src={ln_spec_dev} alt="ln_spec_dev" />
-            <h3 className='text-brand-primary font-semibold text-4xl'>For Lightning spec devs</h3>
-            <p className='text-2xl'>Test out how new features to lightning network are going to perform under specific scenarios</p>
+            <h3 className="text-brand-primary font-semibold text-4xl">
+              For Lightning spec devs
+            </h3>
+            <p className="text-2xl text-gray-700">
+              Easily test protocol changes against realistic network conditions.
+            </p>
           </div>
-          <div className='flex flex-col basis-1/2 gap-2'>
+          <div className="flex flex-col basis-1/2 gap-2">
             <Image src={app_dev} alt="app_dev" />
-            <h3 className='text-brand-primary font-semibold text-4xl'>For app devs</h3>
-            <p className='text-2xl'>Stress test your lightning enabled application to see how it performs with more real-world like scenarios</p>
+            <h3 className="text-brand-primary font-semibold text-4xl">
+              For app devs
+            </h3>
+            <p className="text-2xl text-gray-700">
+              Stress test your lightning enabled application to see how it
+              performs in real-world, liquidity constrained environments.
+            </p>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row w-full gap-4">
-          <div className='flex flex-col basis-1/2 gap-2'>
-            <Image src={bolt} alt="bolt" />
-            <h3 className='text-brand-primary font-semibold text-4xl'>Features</h3>
-            <p className='text-brand-primary font-semibold text-2xl'>Avoid building RPC scripts</p>
-            <p className='text-2xl'>No more writing custom RCP scripts every time you need to simulate traffic!</p>
+      </section>
+      <section className="flex flex-col w-full px-10 py-10 max-w-7xl gap-4 mb-10">
+        <div className="flex flex-col basis-1/2 gap-2">
+          <Image src={bolt} alt="bolt" />
+          <h3 className="text-brand-primary font-semibold text-4xl">
+            Features
+          </h3>
+        </div>
+        <div className="flex flex-col md:flex-row w-full gap-4 font-light text-xl">
+          <div className="flex flex-col basis-1/2 gap-2">
+            <p className="text-brand-primary font-semibold text-2xl">
+              Random Activity Generation
+            </p>
+            <p className="">
+              Simply provide the simulator with a set of nodes and it will
+              generate activity based on the topology of the network.
+            </p>
           </div>
-          <div className='flex flex-col basis-1/2 gap-2'>
-            <div className='md:h-[64px]'></div>
-            <h3 className='text-brand-primary font-semibold text-4xl md:h-[32px]'></h3>
-            <p className='text-brand-primary font-semibold text-2xl'>Execute patterns of payments</p>
-            <p className='text-2xl'>Define specific patterns of payments to simulate the exact kind of traffic you need.</p>
+          <div className="flex flex-col basis-1/2 gap-2">
+            <p className="text-brand-primary font-semibold text-2xl">
+              Configured Payment Flows
+            </p>
+            <p className="">
+              Define specific patterns of payments to simulate the exact kind of
+              traffic you need.
+            </p>
+          </div>
+          <div className="flex flex-col basis-1/2 gap-2">
+            <p className="text-brand-primary font-semibold text-2xl">
+              Cross-Implementation Support
+            </p>
+            <p className="">
+              Support for LND and CLN nodes, with work ongoing to add Eclair and
+              LDK-node.
+            </p>
           </div>
         </div>
       </section>
     </main>
-  )
+  );
 }
