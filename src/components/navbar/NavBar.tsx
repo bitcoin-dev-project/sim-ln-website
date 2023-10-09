9dc900cab3cffb2cb9bab9b0b1310d4cde723d84
Emmanuel-Develops <itakpeemma@gmail.com>
2023-10-09 18:14:42 +0100

    fix: priority hero image and remove nav top and side borders

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='bg-white border border-b-[#E6E6E6] shadow-sm flex justify-center'>
+    <div className='bg-white border border-transparent border-b-[#E6E6E6] shadow-sm flex justify-center'>
       <div className="flex justify-between py-3 px-5 md:px-10 items-center w-full max-w-7xl">
         <Link href="/" className="text-2xl font-press text-brand-primary">SimLN</Link>
         <div className='flex gap-4'>
